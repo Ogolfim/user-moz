e5fa89e200b2d25c365fcf99dc4221f24d9a2a1e
unknown <arlindojosboa@gmail.com>
2022-01-26 12:32:04 +0200

    Password

--- src/workflow/create_user/domain/requiredFields/is/isPassword.ts
+++ src/workflow/create_user/domain/requiredFields/is/isPassword.ts
@@ -1,9 +1,9 @@
-// Check a password between 7 to 15 
+// Check a password between 8 to 15 
 // characters which contain at least
 // one numeric digit and a special character
 
 export const isPassword = (value: string) => {
-  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&_?]).{8,10}$/g
+  const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-_]).{7,15}$/
 
   if(!value.match(regex)) {
     return false

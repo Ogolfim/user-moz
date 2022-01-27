985eefd6d003c9ca86ae27e56147b21009eca873
Arlindo Boa <arlindojosboa@gmail.com>
2022-01-27 22:47:53 +0200

    Verify password

--- src/workflow/account/logIn/use_form/services/log_user.ts
+++ src/workflow/account/logIn/use_form/services/log_user.ts
@@ -4,11 +4,24 @@ import { pipe } from 'fp-ts/lib/function'
 import { createAccessToken } from './create_id_token'
 import { LogUser } from '../user_cases/LogUser'
 import { Name } from '../domain/requiredFields/Name'
+import { verifyPassword } from './verify_password'
 
 export const logUser: LogUser = ({ email, password}) => {
 
   const user = pipe(
     findUser(email),
+    TE.chain(user => {
+      return TE.tryCatch(
+        async () => {
+          const result = await verifyPassword(password, user.hash)
+
+          if(!result) throw new Error('Oops! Senha incorreta')
+
+          return user
+        },
+        (error) => error as Error
+      )
+    }),
     TE.map(user => {
 
       const id_token = createAccessToken({id: user.id})

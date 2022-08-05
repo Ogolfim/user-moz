4f6f39948dd586c2155d974b240f573500be0c40
Arlindo Boa <arlindojosboa@gmail.com>
2022-08-05 21:22:49 +0300

    FEATURE: Creating Excel File

--- src/texts/domain/requiredFields/excel.py
+++ src/texts/domain/requiredFields/excel.py
@@ -1,29 +1,16 @@
-from datetime import date
 from pydantic import BaseModel
 
-class FileData(BaseModel):
-  x: date
-  y: float
-
 class FileBody(BaseModel):
-  data: list[FileData]
+  data: list[list[str, float]]
   
   class Config:
     schema_extra = { 
       "example": {
-        "data": [
-          {
-            "x": "2022-02-01",
-            "y": 0
-          },
-          {
-            "x": "2022-03-01",
-            "y": 4
-          },
-          {
-            "x": "2022-04-01",
-            "y": 2
-          }
+        "data": [ 
+          ['Name', 'Profession', 'Class'],
+          ['Derek', 'Software Developer', 'A'],
+          ['Steve', 'Software Developer', 'B'],
+          [7674, 8849, 99]
         ]
       }
     }
--- src/texts/services/excel.py
+++ src/texts/services/excel.py
@@ -1,5 +1,30 @@
-from src.texts.domain.requiredFields.excel import FileData
+import xlsxwriter
 
-def createExcelFile(fileData: list[FileData], path: str):    
+def createExcelFile(fileData: list[list[str, float]], path: str): 
+    workbook = xlsxwriter.Workbook(path)
+    worksheet = workbook.add_worksheet('data')
+
+    headerFormat = workbook.add_format({
+        'bold': True,
+        'bg_color': '#0c4a6e',
+        'border': 1,
+        'border_color': '#0f172a',
+        'font_color': '#ffffff',
+        'center_across': True,
+    })
+    
+    #                   row, column, Data, Format
+    worksheet.write_row(0, 1, fileData[0], headerFormat)
+    
+    index = 0
+    for line in fileData: 
+        if (index != 0):
+            worksheet.write_row(index, 1, line)
+            
+        index = index + 1
+
+
+    worksheet.set_row(0, 30)
+    workbook.close()   
     
     return path

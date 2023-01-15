df8e312d125667dfaf59e1d69c927401e827f612
Arlindo <arlindojosboa@gmail.com>
2023-01-15 03:18:43 +0200

    SmartSheet static files

--- src/core/infra/http/app.ts
+++ src/core/infra/http/app.ts
@@ -2,9 +2,11 @@ import { billRouter } from '@bill/infra/http/routes'
 import cors from '@fastify/cors'
 import helmet from '@fastify/helmet'
 import { fastifySchedule } from '@fastify/schedule'
+import fastifyStatic from '@fastify/static'
 import { userRouter } from '@user/infra/http/routes'
 import { config } from 'dotenv'
 import fastify from 'fastify'
+import { resolve } from 'path'
 config()
 
 const app = fastify()
@@ -12,6 +14,10 @@ const app = fastify()
 app.register(helmet)
 app.register(cors)
 app.register(fastifySchedule)
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '..', '..', '..', 'smartSheet'),
+  prefix: '/v1/sheet/'
+})
 
 app.register(userRouter)
 app.register(billRouter)
